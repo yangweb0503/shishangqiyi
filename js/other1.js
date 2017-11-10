04835b3eff220a1d3f9cff0996f4561aea2055f2
young <yang10177101@163.com>
2017-11-10 21:40:12 +0800

    3.5

---
+++ js/other1.js
@@ -0,0 +1,53 @@
+
+
+$(function(){
+	$.ajax({
+	type:"get",
+	url:"../json/word.json",
+	async:true,
+	success:function(data){
+		$("#cnt .word img").each(function(n){
+			$(this).attr({src:data[n]});
+			});
+		}
+	});	
+	
+	$.ajax({
+		type:"get",
+		url:"../json/banner.json",
+		async:true,
+		success:function(data){
+			$("table img").each(function(n){
+				var obj = data[n];
+				var str = obj.img[1].src;
+				$(this).attr({src:str});
+			});
+		}
+	});
+	
+
+	$.ajax({
+		type:"get",
+		url:"../json/goods.json",
+		async:true,
+		success:function(data){
+			
+			$("table .tdhover .span1").each(function(n){
+				$(this).text(data[n].img[2].introduce1)
+			});
+			$("table .tdhover .span2").each(function(i){
+				$(this).text(data[i].img[3].introduce2)
+			});
+			$("table .tdhover .span3").each(function(j){
+				$(this).text(data[j].img[5].zhehou)
+			});
+			$("table .tdhover .shange").each(function(k){
+				$(this).text(data[k].img[4].zheqian)
+			});
+		}
+	});
+	
+	
+	
+	
+});
